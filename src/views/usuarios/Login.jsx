f331ddfe33199a5190a6bf2966d1eea710e56d13
claudiagoss85 <claudiagos85@gmail.com>
2024-10-11 17:03:17 -0600

    responsive?

--- src/views/usuarios/Login.jsx
+++ src/views/usuarios/Login.jsx
@@ -55,66 +55,68 @@ export default function Login() {
 
   return (
     <>
-      <h1 className="text-5xl font-black text-white">Iniciar Sesión</h1>
-      <p className="text-2xl font-light text-white mt-5">
-        Empieza a controlar mejor el agua potable. {''}
-        <span className="text-sky-500 font-bold">Iniciando sesión en este formulario</span>
-      </p>
+      <div className="max-w-lg mx-auto p-4 bg-white shadow-lg rounded-lg">
+        <h1 className="text-4xl font-black text-gray-800 text-center">Iniciar Sesión</h1>
+        <p className="text-lg font-light text-gray-600 mt-5 text-center">
+          Empieza a controlar mejor el agua potable. {' '}
+          <span className="text-sky-500 font-bold">Iniciando sesión en este formulario</span>
+        </p>
 
-      <form
-        onSubmit={handleSubmit(handleLogin)}
-        className="space-y-8 p-10 mt-10 bg-white"
-        noValidate
-      >
-        <div className="flex flex-col gap-5">
-          <label className="font-normal text-2xl">Email</label>
-          <input
-            id="email"
-            type="email"
-            placeholder="Email de Registro"
-            className="w-full p-3 border-gray-300 border"
-            {...register("email", {
-              required: "El Email es obligatorio",
-              pattern: {
-                value: /\S+@\S+\.\S+/,
-                message: "E-mail no válido",
-              },
-            })}
-          />
-          {errors.email && (
-            <ErrorMessage>{errors.email.message}</ErrorMessage>
-          )}
-        </div>
-
-        <div className="flex flex-col gap-5">
-          <label className="font-normal text-2xl">Password</label>
-          <div className="relative w-full">
+        <form
+          onSubmit={handleSubmit(handleLogin)}
+          className="space-y-8 p-5 mt-5"
+          noValidate
+        >
+          <div className="flex flex-col gap-5">
+            <label className="font-normal text-lg">Email</label>
             <input
-              type={showPassword ? "text" : "password"}  // Cambiar el tipo de input según el estado
-              placeholder="Password de Registro"
-              className="w-full p-3 border-gray-300 border"
-              {...register("password", {
-                required: "El Password es obligatorio",
+              id="email"
+              type="email"
+              placeholder="Email de Registro"
+              className="w-full p-3 border-gray-300 border rounded-md sm:text-sm md:text-base"
+              {...register("email", {
+                required: "El Email es obligatorio",
+                pattern: {
+                  value: /\S+@\S+\.\S+/,
+                  message: "E-mail no válido",
+                },
               })}
             />
-            <span
-              className="absolute right-3 top-3 cursor-pointer"
-              onClick={() => setShowPassword(!showPassword)}  // Alternar el estado
-            >
-              {showPassword ? <FaEyeSlash /> : <FaEye />}  {/* Cambiar el ícono */}
-            </span>
+            {errors.email && (
+              <ErrorMessage>{errors.email.message}</ErrorMessage>
+            )}
           </div>
-          {errors.password && (
-            <ErrorMessage>{errors.password.message}</ErrorMessage>
-          )}
-        </div>
 
-        <input
-          type="submit"
-          value='Iniciar Sesión'
-          className="bg-sky-600 hover:bg-sky-700 w-full p-3 text-white font-black text-xl cursor-pointer"
-        />
-      </form>
+          <div className="flex flex-col gap-5">
+            <label className="font-normal text-lg">Password</label>
+            <div className="relative w-full">
+              <input
+                type={showPassword ? "text" : "password"}  // Cambiar el tipo de input según el estado
+                placeholder="Password de Registro"
+                className="w-full p-3 border-gray-300 border rounded-md sm:text-sm md:text-base"
+                {...register("password", {
+                  required: "El Password es obligatorio",
+                })}
+              />
+              <span
+                className="absolute right-3 top-3 cursor-pointer"
+                onClick={() => setShowPassword(!showPassword)}  // Alternar el estado
+              >
+                {showPassword ? <FaEyeSlash /> : <FaEye />}  {/* Cambiar el ícono */}
+              </span>
+            </div>
+            {errors.password && (
+              <ErrorMessage>{errors.password.message}</ErrorMessage>
+            )}
+          </div>
+
+          <input
+            type="submit"
+            value='Iniciar Sesión'
+            className="bg-sky-600 hover:bg-sky-700 w-full p-3 text-white font-black text-lg sm:text-base cursor-pointer rounded-md"
+          />
+        </form>
+      </div>
     </>
   );
 }
